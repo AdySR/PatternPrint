3eee231ee98cf2d5463803c6dc22154f3d6f7367
Adatix <rathi.ady@gmail.com>
2022-08-11 12:36:28 +0530

    init comm

---
+++ PatternPrint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class PatternPrint
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Input the depth of star");
+        int n = Convert.ToInt32(Console.ReadLine());
+        pattern1(n);
+    }
+
+    static void pattern1(int n)
+        //*
+        //**
+        //***
+        //****
+        //*****
+        //******
+    {
+        for (int row=1; row<=n; row+=1)
+        {
+            for(int col=1; col<=row; col++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+
+        }
+    }
+}
