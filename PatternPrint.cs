bab1bad9d5daf4c264908e5d160ceba8deed3e20
Adatix <rathi.ady@gmail.com>
2022-08-14 18:39:51 +0530

    added new patterns

--- PatternPrint.cs
+++ PatternPrint.cs
@@ -4,9 +4,13 @@ class PatternPrint
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Input the depth of star");
-        int n = Convert.ToInt32(Console.ReadLine());
-        pattern1(n);
+        //Console.WriteLine("Input the depth of star");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        ////pattern1(n);
+        //roughPad(5);
+        //pattern1(6);
+        //pattern2();
+        Pattern3();
     }
 
     static void pattern1(int n)
@@ -25,6 +29,51 @@ class PatternPrint
             }
             Console.Write("\n");
 
+        }
+    }
+    static void roughPad(int n)
+    {
+        for (int row = 1; row <= n; row += 1)
+        {
+            Console.Write("*"+"\n");
+        }
+    }
+    
+    static void pattern2()
+    {
+        /*
+        12345
+        12345
+        12345
+        12345
+         */
+    for (int i=1;i<=5;i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    static void Pattern3()
+    {
+        /*
+        11111
+        22222
+        33333
+        44444
+        55555
+        */
+        for (int i = 1; i <= 5; i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
         }
     }
 }
