5d3d067bed0e9f5c8839edcbb058b77679a1a45b
Adatix <rathi.ady@gmail.com>
2022-08-14 19:48:42 +0530

    added new patterns

--- PatternPrint.cs
+++ PatternPrint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 class PatternPrint
 {
@@ -11,7 +12,10 @@ class PatternPrint
         //pattern1(6);
         //pattern2();
         //Pattern3();
-        Pattern4();
+        //Pattern4();
+        //Pattern5();
+        //Pattern6();
+        Pattern7();
     }
 
     static void pattern1(int n)
@@ -93,5 +97,65 @@ class PatternPrint
             }
             Console.WriteLine();
         }
+    }
+
+    static void Pattern5()
+    {
+        /*
+        A B C D E
+        A B C D E
+        A B C D E
+        A B C D E
+         */
+        string a = "A";
+        byte[] asciibyte = Encoding.ASCII.GetBytes(a);
+        int b = char.ConvertToUtf32("A", 0);
+        //Console.WriteLine(b);
+
+
+        for (int i = 1; i < 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(char.ConvertFromUtf32((j + 64)) + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    static void Pattern6()
+    {
+        //5 4 3 2 1
+        //5 4 3 2 1
+        //5 4 3 2 1
+        //5 4 3 2 1
+        //5 4 3 2 1
+        for (int i=1;i<=5;i++)
+        {
+            for(int j=5;j>0;j--)
+            {
+                Console.Write(j+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Pattern7()
+    {
+    /*  1
+        12
+        123
+        1234
+        12345
+    */
+        for (int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i; j++)
+            { Console.Write(j); }
+            
+            Console.WriteLine();
+        }
+        
     }
 }
